a6158a56e033e6f2f034f2aac03f7062e6046d53
sabotajj <sabih.erdemanar@gmail.com>
2023-10-04 22:06:47 +0300

    code cleaning

---
+++ shortcutCreator/shortcutCreator/FileUtilities.cs
@@ -0,0 +1,57 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+
+
+namespace shortcutCreator
+{
+    public class FileUtilities
+    {
+        public static bool IsFileSupported(string[] supportedFileTypes, string fileName)
+        {
+            return supportedFileTypes.Any(ext => "." + ext == Path.GetExtension(fileName).ToLower());
+        }
+        public static string GetFileFullPath(string fileName, List<string> fileList)
+        {
+            return fileList.FirstOrDefault(file => Path.GetFileName(file).ToLower() == fileName.ToLower());
+        }
+        public static List<string> GetAllFilesPathRecursive(string rootPath)
+        {
+            var filesList = new List<string>();
+            filesList.AddRange(
+                GetFilesList(rootPath).ToList()
+            );
+            // if there are still directories
+            var subDirs = GetFolderList(rootPath);
+            foreach (string subDir in subDirs)
+            {
+                filesList.AddRange(GetAllFilesPathRecursive(subDir));
+            }
+            return filesList;
+        }
+        public static string[] GetFolderList(string filePath)
+        {
+            var currentDirs = Directory.GetDirectories(filePath);
+            return currentDirs;
+        }
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = targetFileLocation;   // The description of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Save();                                    // Save the shortcut
+        }
+        public static string[] GetFilesList(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath);
+        }
+    }
+}
--- shortcutCreator/shortcutCreator/Program.cs
+++ shortcutCreator/shortcutCreator/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using IWshRuntimeLibrary;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
 
 namespace shortcutCreator
 {
@@ -13,63 +14,38 @@ namespace shortcutCreator
         static void Main(string[] args)
         {
             string[] fileTypeList = new string[] { "jpg", "jpeg", "mov", "mpg", "mp4", "avi" };
-            var AllFilesInSourceDir = getAllFilesListWithPath(args[0]);
-            var AllFilesInDestDir = getAllFilesListWithPath(args[1]);
+            var AllFilesInSourceDir = FileUtilities.GetAllFilesPathRecursive(args[0]);
+            var AllFilesInDestDir = FileUtilities.GetAllFilesPathRecursive(args[1]);
             int i = 0;
-            foreach(var sourceFile in AllFilesInSourceDir)
+            foreach (var sourceFile in AllFilesInSourceDir)
             {
-                Console.WriteLine(++i+"/"+AllFilesInSourceDir.Count);
-                if (!fileTypeList.Any(ext => "."+ext == Path.GetExtension(sourceFile).ToLower())) continue;
+                Console.WriteLine($@"Working on file {sourceFile} ({i} / {AllFilesInSourceDir.Count})");
+
+                if (!FileUtilities.IsFileSupported(fileTypeList, sourceFile))
+                    continue;
+
                 string fileName = Path.GetFileName(sourceFile);
-                var fileFullPath = findFile(fileName, AllFilesInDestDir) ;
+                var fileFullPath = FileUtilities.GetFileFullPath(fileName, AllFilesInDestDir);
+                
                 if (fileFullPath == null) continue;
-                else
+
+                FileUtilities.CreateShortcut(fileName,
+                    Path.GetDirectoryName(sourceFile),
+                    fileFullPath
+                );
+
+                Console.WriteLine($@"Deleting file {sourceFile}");
+                try
                 {
-                    createShortcut(fileName,
-                        Path.GetDirectoryName(sourceFile),
-                        fileFullPath
-                    );
                     System.IO.File.Delete(sourceFile);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($@"Error while deleting file {sourceFile}");
                 }
             }
         }
-        private static string findFile(string fileName, List<string> fileList)
-        {
-            return fileList.Where(file => Path.GetFileName(file).ToLower() == fileName.ToLower()).FirstOrDefault();
-        }
-        private static List<string> getAllFilesListWithPath(string rootPath)
-        {
-            var filesList = new List<string>();
-            filesList.AddRange(
-                getFilesInDir(rootPath).ToList()
-            );
-            // if there are still directories
-            var subDirs = getFolderListInDir(rootPath);
-            foreach(string subDir in subDirs)
-            {
-                filesList.AddRange(getAllFilesListWithPath(subDir));
-            }
-            return filesList;
-        }
-        private static string[] getFolderListInDir(string filePath)
-        {
-            var currentDirs = Directory.GetDirectories(filePath);
-            return currentDirs;
-        }
-        private static void createShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
-        {
-            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
-            WshShell shell = new WshShell();
-            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
-
-            shortcut.Description = targetFileLocation;   // The description of the shortcut
-            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
-            shortcut.Save();                                    // Save the shortcut
-        }
-        private static string[] getFilesInDir(string directoryPath)
-        {
-            return Directory.GetFiles(directoryPath);   
-        }
+        
         
     }
 }
